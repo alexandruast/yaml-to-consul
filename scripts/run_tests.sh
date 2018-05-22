a5fb5946d936bd2a8de6851a56cff5df17a226da
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-22 16:11:02 +0300

    Work in progress

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
-set -euEo pipefail
-trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
+#!/usr/bin/env sh
+set -ex
 cd /project
 wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip
 unzip consul_1.0.6_linux_amd64.zip
