6df78b27f1b558fc687adfbfecea6ee8d99a8e56
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-22 16:09:44 +0300

    Work in progress

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euEo pipefail
 trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-docker run -it --rm -v "./:/project" python:3.6-alpine /project/scripts/run_tests.sh
+docker run -it --rm -v "$(pwd):/project" python:3.6-alpine /project/scripts/run_tests.sh
