fa43ac7d27ddff99a1e283c9a1b9e882c736262e
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-22 16:22:16 +0300

    Work in progress

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,6 +2,7 @@
 set -ex
 cd /project
 apk add -q --no-cache curl unzip
+ls -la
 curl -LSs https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip -o consul_1.0.6_linux_amd64.zip
 unzip consul_1.0.6_linux_amd64.zip
 nohup ./consul agent -dev
