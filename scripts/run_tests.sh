5c2678d6b12823b19640c18a2022ff1ca059d10a
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-22 16:26:33 +0300

    Work in progress

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,10 +2,9 @@
 set -ex
 cd /project
 apk add -q --no-cache curl unzip
-ls -la
-curl -LSs https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip -o consul_1.0.6_linux_amd64.zip
-unzip consul_1.0.6_linux_amd64.zip
-nohup ./consul agent -dev
+curl -LSs https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip -o /tmp/consul_1.0.6_linux_amd64.zip
+unzip /tmp/consul_1.0.6_linux_amd64.zip
+nohup /tmp/consul agent -dev
 pip install -r requirements.txt
 python tests/test_kvstore.py
 python kvstore.py
